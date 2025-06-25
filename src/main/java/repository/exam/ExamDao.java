c41c9cc5dbde2a8b6d36e274360ffdcdf20b655d
Master-Long-3112 <trinhhuylong2005@gmail.com>
2025-06-26 00:16:50 +0700

    upload

---
+++ src/main/java/repository/exam/ExamDao.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repository.exam;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import model.exam.Exam;
+import repository.FullOptionDAO;
+
+/**
+ *
+ * @author MasterLong
+ */
+public class ExamDao extends FullOptionDAO<Exam> {
+
+    public ExamDao(EntityManagerFactory entityManagerFactory, Class<Exam> entityClass) {
+        super(entityManagerFactory, entityClass);
+    }
+
+    public ExamDao(EntityManagerFactory entityManagerFactory) {
+        super(entityManagerFactory, Exam.class);
+    }
+
+    public Exam findExamByCode(String examCode) {
+        try (EntityManager em = entityManagerFactory.createEntityManager()) {
+            String jpql = "SELECT e FROM " + entityClass.getSimpleName() + " e WHERE e.examCode" + " = :value";
+            return em.createQuery(jpql, entityClass).setParameter("value", examCode).getSingleResult();
+        }
+    }
+
+}
