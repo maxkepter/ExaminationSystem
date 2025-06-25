4d5f83cbedb4bb270526c275739b9e098af69e35
kepter <tuna26hn@gmail.com>
2025-06-25 14:04:35 +0700

    lmao

--- src/main/java/repository/exam/ExamDao.java
+++ src/main/java/repository/exam/ExamDao.java
@@ -4,9 +4,8 @@
  */
 package repository.exam;
 
-import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.TypedQuery;
+import java.util.Map;
 import model.exam.Exam;
 import repository.FullOptionDAO;
 
@@ -14,22 +13,12 @@ import repository.FullOptionDAO;
  *
  * @author MasterLong
  */
-public class ExamDao extends FullOptionDAO<Exam> {
-    public ExamDao(EntityManagerFactory entityManagerFactory, Class<Exam> entityClass) {
+public class ExamDAO extends FullOptionDAO<Exam>{
+    
+    public ExamDAO(EntityManagerFactory entityManagerFactory, Class<Exam> entityClass) {
         super(entityManagerFactory, entityClass);
     }
+    
+    
 
-    public ExamDao(EntityManagerFactory entityManagerFactory) {
-        super(entityManagerFactory, Exam.class);
-    }
-
-    public Exam findExamByCode(String examCode) {
-
-        try (EntityManager em = entityManagerFactory.createEntityManager()) {
-            TypedQuery<Exam> query = em.createQuery(
-                    "SELECT e FROM Exam e WHERE e.examCode = :code", Exam.class);
-            query.setParameter("code", examCode);
-            return query.getResultStream().findFirst().orElse(null);
-        }
-    }
 }
