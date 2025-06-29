2d2ff105dace488bc56098147a21bd8fa0e05112
kepter <tuna26hn@gmail.com>
2025-06-29 22:56:29 +0700

    before merge

--- src/main/java/filter/FilterOrderListener.java
+++ src/main/java/filter/FilterOrderListener.java
@@ -14,17 +14,16 @@ public class FilterOrderListener implements ServletContextListener {
         /* ➊ Đăng ký RememberLoginFilter TRƯỚC */
         FilterRegistration.Dynamic remember = ctx.addFilter("RememberLoginFilter", RememberLoginFilter.class);
         remember.addMappingForUrlPatterns(
-            EnumSet.of(DispatcherType.REQUEST),  // loại dispatcher
-            false,                              // false = chèn mapping Ở ĐẦU chuỗi
-            "/*"                                // pattern
+                EnumSet.of(DispatcherType.REQUEST), // loại dispatcher
+                false, // false = chèn mapping Ở ĐẦU chuỗi
+                "/*" // pattern
         );
 
         /* ➋ Đăng ký ExamInProgressFilter SAU */
         FilterRegistration.Dynamic exam = ctx.addFilter("ExamInProgressFilter", ExamInProgressFilter.class);
         exam.addMappingForUrlPatterns(
-            EnumSet.of(DispatcherType.REQUEST),
-            true,                               // true = chèn mapping Ở CUỐI chuỗi
-            "/*"
-        );
+                EnumSet.of(DispatcherType.REQUEST),
+                true, // true = chèn mapping Ở CUỐI chuỗi
+                "/*");
     }
 }
